7467fec060317c27a00fb27356516280e6bd91c6
M-Kub <mkubesch@gmail.com>
2022-12-30 22:03:54 +0100

    Added two player mode with time meassuring who was the faster player

--- ProjectsLearn/starterBook/reaction_game.py
+++ ProjectsLearn/starterBook/reaction_game.py
@@ -7,21 +7,32 @@ pressed = False
 led = machine.Pin(15, machine.Pin.OUT)
 right_button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
 left_button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
+fastest_button = None
 
 
 
 def button_handler(pin):
     global pressed
+    global timer_reaction
     if not pressed:
         pressed=True
-        timer_reaction = utime.ticks_diff(utime.ticks_ms, timer_start)
-        print(f"Your reaction time was {timer_reaction} milliseconds.")
+        timer_reaction = utime.ticks_diff(utime.ticks_ms(), timer_start)
+        global fastest_button
+        fastest_button = pin
 
 
 led.value(1)
 utime.sleep(urandom.uniform(5, 10))
 led.value(0)
-
 timer_start = utime.ticks_ms()
 right_button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
 left_button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
+
+
+while fastest_button is None:
+    utime.sleep(1)
+if fastest_button is left_button:
+    print(f"{left_button} player wins in {timer_reaction} ms!")
+elif fastest_button is right_button:
+    print(f"{right_button} player wins in {timer_reaction} ms!")
+

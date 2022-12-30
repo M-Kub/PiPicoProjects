68ca252cf7076ea72f2fdf3e59867b8c7ed55d81
M-Kub <mkubesch@gmail.com>
2022-12-30 17:37:36 +0100

    Added second button to reaction game, and measuring reaction time.

--- ProjectsLearn/starterBook/reaction_game.py
+++ ProjectsLearn/starterBook/reaction_game.py
@@ -5,17 +5,23 @@ import urandom
 
 pressed = False
 led = machine.Pin(15, machine.Pin.OUT)
-button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+right_button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+left_button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
 
 
 def button_handler(pin):
     global pressed
     if not pressed:
         pressed=True
-        print(pin)
+        timer_reaction = utime.ticks_diff(utime.ticks_ms, timer_start)
+        print(f"Your reaction time was {timer_reaction} milliseconds.")
 
 
 led.value(1)
 utime.sleep(urandom.uniform(5, 10))
 led.value(0)
-button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
+
+timer_start = utime.ticks_ms()
+right_button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
+left_button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)

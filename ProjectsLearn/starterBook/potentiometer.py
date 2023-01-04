0819a234931241148ba1ebae0e80e7a189b8dd61
M-Kub <mkubesch@gmail.com>
2023-01-04 17:30:01 +0100

    Added LED with PWM to potentiometer

--- ProjectsLearn/starterBook/potentiometer.py
+++ ProjectsLearn/starterBook/potentiometer.py
@@ -3,10 +3,9 @@ import utime
 
 
 potentiometer = machine.ADC(26)
+led = machine.PWM(machine.Pin(15))
+led.freq(1000)
 
-conversion_factor = 3.3 / (65535)
 
 while True:
-    voltage = potentiometer.read_u16() * conversion_factor
-    print(voltage)
-    utime.sleep(3)
+    led.duty_u16(potentiometer.read_u16())

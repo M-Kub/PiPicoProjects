e0a6fee1028bcf532ba79c587ee0efc397ca8c77
M-Kub <mkubesch@gmail.com>
2023-01-19 23:33:37 +0100

    Finished basic server/response setup. To Fix: EADDRINUSE

--- ProjectsLearn/PicoW-WiFi/serverTest.py
+++ ProjectsLearn/PicoW-WiFi/serverTest.py
@@ -14,11 +14,9 @@ wifiSSID = stuff.wifiSSID
 wifiPW = stuff.wifiPW
 rp2.country('DE')
 
-html = """<!doctype html><html lang="en"> \
-<head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="shortcut icon" href="data:"> \
-<title>PicoW SeerverTest</title> \
-</head> \
-<body><h1 align="center">HiHo</h1><hr>TEXT<hr><p align="center">What's up everybody?</p></body></html>"""
+html = """<!doctype html><html lang="en"> <head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="shortcut icon" href="data:"> \
+<title>PicoW ServerTest</title> </head> \
+<body><h1 align="center">HiHo, what is going on?</h1><hr>TEXT<hr><p align="center">What's up everybody?</p></body></html>"""
 
 
 def wifiConnect():
@@ -53,6 +51,51 @@ if ipv4 != "":
     server.bind(addr)
     server.listen(1)
     print(f"Server is listening on {addr}")
+    
+# Waitiing for connections
+while True:
+    try:
+        conn, addr = server.accept()
+        print(f"HTTP request from {addr} !")
+        request = conn.recv(1024)
+        request = str(request)
+        request = request.split()
+        print(f"URL: {request[1]}")
+        if request[1] == "/light/on":
+            print("Turning on LED!")
+            led.value(1)
+        elif request[1] == "/light/off":
+            print('Turning off LED')
+            led.value(0)
+        elif request[1] == "/light/toggle":
+            print("Toggle LED")
+            led.toggle()
+        # Evaluate LED status
+        state_is = ""
+        if led.value() == 1:
+            state_is += "<p align='center'><b>LED is ON</b> <a href='/light/off'><button>OFF</button></a></p>"
+        if led.value() == 0:
+            state_is += '<p align="center"><b>LED is OFF</b> <a href="/light/on"><button>ON</button></a></p>'
+        # Create the HTTP-Response
+        response = html.replace("TEXT", state_is)
+        conn.send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
+        conn.send(response)
+        conn.close()
+        print("HTTP-Response was send\n")
+        print()
+    except OSError as e:
+        break
+    except (KeyboardInterrupt):
+        break
+
+try: conn.close()
+except NameError: pass
+try: server.close()
+except NameError: pass
+print("Server shut down")
+            
+        
+            
         
         
 
